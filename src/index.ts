259e7b2e14613da34eef95d0f231debd909876e5
zwkang <kang95630@gmail.com>
2024-11-20 15:20:43 +0800

    feat: add ignore case

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,10 @@
-/* eslint-disable no-console */
-// const css = require('css')
 import * as css from 'css'
 import cssWhat from 'css-what'
 
-import postcss from 'postcss'
-
-import { createParser } from 'css-selector-parser'
-import { readFile, writeFile } from 'node:fs/promises'
-
-// const ast = css.parse(`#body > .delta + .test { font-size: 12px; } .delta { color: red }`)
-
-// console.log(JSON.stringify(ast))
-// const parse = createParser()
-
-// console.log(
-//   JSON.stringify(parse('#body > g > #d + .delta, .delta')),
-// )
-
-// postcss(`#body > .delta + .test { font-size: 12px; } .delta { color: red }`).process(css, { from})
-
-
-const text = `#body > .delta + .test { font-size: 12px; }`
-
-function combine<T extends Record<string ,any>>(list: T[]) {
+function combine<T extends Record<string, any>>(list: T[]) {
    const result: T[] = []
-   // while()
-   for(const item of list) {
-      if(item?.type === 'attribute' || item.type === 'tag') {
+   for (const item of list) {
+      if (item?.type === 'attribute' || item.type === 'tag') {
          result.push(item);
       } else {
          break;
@@ -42,7 +20,7 @@ function filterPseudoRoot(rule: cssWhat.Selector, prefix: string) {
       return false;
    }
    const regexp = new RegExp(`^${prefix}`)
-   if(rule?.type === 'attribute' && regexp.test(rule.value)) {
+   if (rule?.type === 'attribute' && regexp.test(rule.value)) {
       return false
    }
 
@@ -51,7 +29,7 @@ function filterPseudoRoot(rule: cssWhat.Selector, prefix: string) {
 
 function isMatchPrefixClassName(rule: cssWhat.Selector, prefix: string) {
    const regexp = new RegExp(`^${prefix}`)
-   if(rule?.type === 'attribute' && rule.name === 'class' && regexp.test(rule.value)) {
+   if (rule?.type === 'attribute' && rule.name === 'class' && regexp.test(rule.value)) {
       return true
    }
 
@@ -61,68 +39,112 @@ function isMatchPrefixClassName(rule: cssWhat.Selector, prefix: string) {
 
 type IOptions = {
    prefix?: string;
-   ignoreCss: (string | RegExp)[];
-
+   ignoreCss?: (string | RegExp)[];
+   detail?: boolean;
+   source?: string;
 }
 
 const defaultOptions: IOptions = {
    prefix: '',
-   ignoreCss: []
+   ignoreCss: [],
+   detail: false,
+   source: ''
+}
+
+function filterClass(ignoreCss: (string | RegExp)[]) {
+   const cache = new Map();
+
+   return function (selector: string) {
+      const hasIgnore = ignoreCss.some(item => {
+         if (typeof item === 'string') {
+            if (selector === item) {
+               return true;
+            }
+         }
+         if (item instanceof RegExp) {
+            if (item.test(selector)) {
+               return true;
+            }
+         }
+
+         return false;
+      })
+      if (cache.has(selector)) {
+         return cache.get(selector)
+      }
+      cache.set(selector, hasIgnore);
+      return hasIgnore;
+   }
+}
+
+function makePositionPath(source: string, position: css.Position) {
+   const startLine = position.line;
+   const startColumn = position.column;
+   return `${source}:${startLine}:${startColumn}`
 }
 
 
-export async function analyzeCssFileContent(content: string, options: IOptions ) {
-   const { prefix = 'delta-', ignoreCss = [] } = {
+export async function analyzeCssFileContent(content: string, options: IOptions) {
+   const { prefix = 'delta-', ignoreCss = [], detail, source = '' } = {
       ...defaultOptions,
       ...options
    };
+   const filter = filterClass(ignoreCss);
    const root = await css.parse(content);
    const rules = root.stylesheet?.rules;
-   if(!rules) {
+   if (!rules) {
       return;
    }
-   const result: any[] = [];
+   const result: {
+      selector: string;
+      path?: string;
+      source?: string;
+   }[] = [];
 
-   for(const rule of rules) {
-      if(rule.type === 'rule') {
-         if(!rule.selectors || !rule.declarations) {
+   for (const rule of rules) {
+
+      // console.log(rule)
+      if (rule.type === 'rule') {
+         if (!rule.selectors || !rule.declarations) {
             continue;
          }
 
-         // rule.selectors
-         for(const selector of (rule.selectors ?? [])) {
+         for (const selector of (rule.selectors ?? [])) {
+            if (filter(selector)) {
+               continue;
+            }
             const parsedSelector = cssWhat.parse(selector);
-            
+
             const expressions: cssWhat.Selector[] = [];
-            
+
             for (let i = 0, len = parsedSelector[0].length; i < len; i++) {
                expressions.push(parsedSelector[0][i]);
             }
 
             const needExpressions = combine(expressions);
             const checkExpression = needExpressions.some(item => isMatchPrefixClassName(item, prefix))
-            if(expressions?.[0] && !filterPseudoRoot(expressions[0], prefix)) {
+            if (expressions?.[0] && !filterPseudoRoot(expressions[0], prefix)) {
                continue;
             }
-            console.log(needExpressions)
-            if(checkExpression) {
+            if (checkExpression) {
                continue;
             }
-
-            result.push(cssWhat.stringify(parsedSelector));
+            result.push({
+               selector: cssWhat.stringify(parsedSelector),
+               path: detail && source ? makePositionPath(source, rule.position!.start!) : '',
+               source: source,
+            })
          }
       }
    }
 
-   return Array.from(new Set(result.filter(Boolean)));
-}
-
-; (async () => {
-   const result = (await readFile('./src/d.css')).toString('utf-8')
-   const res = await analyzeCssFileContent(result, {
-      prefix: 'delta-',
-      ignoreCss: [/CodeMirror/]
+   const tempCache = new Map();
+   const list = result.filter(item => {
+      if (tempCache.has(item.selector)) {
+         return false;
+      }
+      tempCache.set(item.selector, true);
+      return true;
    })
-
-   console.log(res?.filter(item => !/CodeMirror|cm-|t-icon/.test(item)));
-})()
+   return list;
+}
